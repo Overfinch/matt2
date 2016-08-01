2c6ee1ebf3c2eb2d7673f3118dcc0a262113ffba
Overfinch <bundichmail@gmail.com>
2016-08-01 18:20:00 +0300

    XML
    Сделал рекурсивную функцию которая генерирует xml дерево с многомерных
    вложенных ассоциативных массивов

--- xml/index.php
+++ xml/index.php
@@ -1,5 +1,5 @@
 <?php
-
+echo "<pre>";
 
 function readParams($sourceFile){ // вывод содержимого XML
         $xml = simplexml_load_file($sourceFile);
@@ -15,15 +15,28 @@ function writeParams($sourceFile, $params, $gameName){
             $game = $xml->xpath("/list/games/game[@name=\"$gameName\"]"); // и записываем его xpath
         }
 
-        foreach ($params as $key => $val) { // перебираем параметры
-            if($game[0]->$key){ // если такой нод есть..
+        createNods($game,$params);
+
+        file_put_contents($sourceFile, $xml->asXML()); // записываем наш объект $xml в xml файл
+}
+
+function createNods($game, $params){
+    foreach ($params as $key => $val) { // перебираем параметры
+        if($game[0]->$key){ // если такой нод есть..
+            if(is_array($val)){ // если значение это массив, значит это дочерние ноды...
+                createNods($game[0]->$key, $val); // рекурсивно вызываем эту же функцию, но к Xpath ($game[0]) дописываем ->$key, это будет путь к дочернему ноду в который мы запишем $val
+            }else{
                 $game[0]->$key = $val; // записываем в него значение
-            }else{ // если нету...
+            }
+        }else{ // если нету...
+            if(is_array($val)){  // если значение это массив, значит это будут новые дочерние ноды...
+                $game[0]->addChild($key," "); // создаём новый пустой нод
+                createNods($game[0]->$key, $val); // а потом записываем в него $val, но передаём Xpath $game[0] вместе с ->$key, это xpath к толькочто созданому ноду
+            }else{
                 $game[0]->addChild($key, $val); // то создаём его и записываем в него значение
             }
         }
-
-        file_put_contents($sourceFile, $xml->asXML()); // записываем наш объект $xml в xml файл
+    }
 }
 
 
@@ -31,9 +44,11 @@ function writeParams($sourceFile, $params, $gameName){
 
 
 $file = "config.xml"; // путь к файлу
-$params = ['name' => 'Witcher', "ganre" => "RPG", "developer" => "CD project"]; // параметры
+$params = ['name' => 'Witcher', "ganre" => "RPG", "developer" => "CD project", "persons" => ["main_hero" => "Geralt", "secondary_hero" => "Ciry"]]; // параметры
 $gameName = "Witcher"; // имя
 
 writeParams($file,$params,$gameName);
 $output = readParams($file);
 print $output;
+
+echo "</pre>";
