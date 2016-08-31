93f80ef9c3df9c49101160d1150b706b6f6d4b61
Overfinch <bundichmail@gmail.com>
2016-08-31 20:13:54 +0300

    20-factory-method

--- 20-factory-method/index.php
+++ 20-factory-method/index.php
@@ -1,5 +1,7 @@
 <?php
 
+// Реализация шаблона Factory Method
+
 abstract class ApptEncoder {
     abstract function encode();
 }
@@ -16,35 +18,52 @@ class MegaApptEncoder extends ApptEncoder {
     }
 }
 
-class CommsManager {
-    const BLOGS = 1;
-    const MEGA = 2;
-    private $mode = 1;
+abstract class CommsManager {
+    abstract function getHeaderText();
+    abstract function getFooterText();
+    abstract function getApptEncoder();
+}
 
-    function __construct($mode){
-        $this->mode = $mode;
+class BlogsCommsManager extends CommsManager {
+    function getHeaderText(){
+                return "BlogsCall верхний колонтикул <br>";
     }
 
-    function getHeaderText(){
-        switch ($this->mode){
-            case (self::MEGA):
-                return "MegaCall верхний колонтикул <br>";
-            default:
-                return "BlogsCall верхний колонтикул <br>";
-        }
+    function getFooterText(){
+        return "BlogsCall нижний колонтикул <br>";
     }
 
     function getApptEncoder(){
-        switch ($this->mode){
-            case (self::MEGA):
-                return new MegaApptEncoder();
-            default:
-                return new BlogsApptEncoder();
-        }
+        return new BlogsApptEncoder();
     }
 }
 
-$comms = new CommsManager(CommsManager::MEGA);
-$apptEncoder = $comms->getApptEncoder();
-print $apptEncoder->encode();
-print $comms->getHeaderText();
+class MegaCommsManager extends CommsManager {
+    function getHeaderText(){
+        return "MegaCall верхний колонтикул <br>";
+    }
+
+    function getFooterText(){
+        return "MegaCall нижний колонтикул <br>";
+    }
+
+    function getApptEncoder(){
+        return new MegaApptEncoder();
+    }
+}
+
+// Если нам надо будет добавить новые дочерние классы для CommsManager или ApptEncoder
+// то мы их просто допишем, а клиентский код, уже будет готов принять их,
+// так как в абстрактных классах (CommsManager и ApptEncoder) описаны методы, и клиентский код
+// будет знать как с ними работать, ему не нужно знать какой именно дочерний класс он обрабатывает
+// главное что он наследует абстрактные классы, и можно создавать много дочерних классов, не меняя при это клиентский код
+
+$blogManager = new BlogsCommsManager();
+print $blogManager->getHeaderText();
+print $blogManager->getFooterText();
+print $blogManager->getApptEncoder()->encode(); // тут мы получаем объект типа ApptEncoder или его наследников
+                                                // (в данном случае BlogsApptEncoder), нам не обязательно знать какой именно
+                                                // подкласс будет веозвращен, главное что мы уверены что он наследник ApptEncoder
+                                                // в дальнейшем мы можем добавить новые дочерние классы которые будут расширять ApptEncoder
+                                                // но тут ничего менять не придётся
+
