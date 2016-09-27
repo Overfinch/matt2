aafe6d22bd9edc8d552c444eda64d7f947c005c9
Overfinch <bundichmail@gmail.com>
2016-09-27 19:44:18 +0300

    22-composite

--- 22-composite/index.php
+++ 22-composite/index.php
@@ -1,25 +1,28 @@
 <?php
 // Реализация шаблона Composite
 
+class UnitException extends Exception {}
+
 abstract class Unit {
     abstract function bombardStrength();
-    abstract function addUnit(Unit $unit);
-    abstract function removeUnit(Unit $unit);
+
+    function addUnit(Unit $unit){
+        throw new UnitException(self::class.' относится к листьям');
+    }
+    function removeUnit(Unit $unit){
+        throw new UnitException(self::class.' относится к листьям');
+    }
+    // исключения на случай если к "листьям" попробуют добавить или удалить что-то
+    // а для "композитов" будут обьявлены рабочие методы addUnit и removeUnit
 }
 
 class Archer extends Unit { // листья
-    function addUnit(Unit $unit){}
-    function removeUnit(Unit $unit){}
-
     function bombardStrength(){
         return 4;
     }
 }
 
 class LaserCannonUnit extends Unit { // листья
-    function addUnit(Unit $unit){}
-    function removeUnit(Unit $unit){}
-
     function bombardStrength(){
         return 44;
     }
@@ -27,7 +30,6 @@ class LaserCannonUnit extends Unit { // листья
 
 class Army extends Unit{ // композит который может в себе объекты типа Unit
     private $units = [];
-    private $armies = [];
 
     function addUnit(Unit $unit){ // добавление объекта типа Unit
         if(in_array($unit, $this->units, true)){
@@ -40,34 +42,37 @@ class Army extends Unit{ // композит который может в себ
         $this->units = array_udiff($this->units, [$unit], function($a,$b){ return ($a === $b)?0:1; });
     }
 
-    function addArmy(Army $army){ // добавление объекта типа Army
-        array_push($this->armies, $army);
-    }
-
     function bombardStrength(){ // возвращает силу всех Unit и Army объектов, которые находятся в нём
         $ret = 0;
         foreach ($this->units as $unit){
             $ret += $unit->bombardStrength();
         }
-
-        foreach ($this->armies as $army){
-            $ret += $army->bombardStrength();
-        }
-
         return $ret;
     }
-
 }
 
-// классы Army и TroopCarrier - это композиты;
+// класс Army - это композит (их тоже может быть много, как и листьев);
 // Они предназначены для того что бы содержать в себе объекты типа Unit.
 // Классы Archer и LaserCannon - это листья, предназначены для того что бы
 // поддерживать операции с объектами типа Unit, и в них не могут содержатся другие объекты типа Unit
 
-$archer1 = new Archer();
-$LaserCannonUnit1 = new LaserCannonUnit();
+// создадим армию
+$main_army = new Army();
 
-$army = new Army();
-$army->addUnit($archer1);
-$army->addUnit($LaserCannonUnit1);
-print $army->bombardStrength();
+// добавим пару боевых едениц
+$main_army->addUnit(new Archer());
+$main_army->addUnit(new LaserCannonUnit());
+
+// создадим ещё армию
+$sub_army = new Army();
+
+//добавим ей несколько боевых едениц
+$sub_army->addUnit(new Archer());
+$sub_army->addUnit(new Archer());
+$sub_army->addUnit(new Archer());
+
+// добавим вторую армию к первой
+$main_army->addUnit($sub_army);
+
+// Все вычисления выполняются за кулисами
+print $main_army->bombardStrength();
