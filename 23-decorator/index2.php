174a5fd5730943f5bec2832075da66f62063e336
Overfinch <bundichmail@gmail.com>
2017-01-18 19:12:19 +0200

    23-decorator

--- 23-decorator/index2.php
+++ 23-decorator/index2.php
@@ -1,3 +1,49 @@
 <?php
 
-echo "test";
+class RequestHelper {}
+
+abstract class ProcessRequest {
+    abstract function process(RequestHelper $req);
+}
+
+class MainProcess extends ProcessRequest {
+    function process(RequestHelper $req){
+        print __CLASS__." Выполнение запроса <br>";
+    }
+}
+
+abstract class DecorateProcess extends ProcessRequest {
+    protected $processRequest;
+
+    function __construct(ProcessRequest $processRequest) {
+        $this->processRequest = $processRequest;
+    }
+}
+
+class LogRequest extends DecorateProcess {
+    function process(RequestHelper $req){
+        print __CLASS__." Регистрация запроса <br>";
+        $this->processRequest->process($req);
+    }
+}
+
+class AuthenticateProcess extends DecorateProcess {
+    function process(RequestHelper $req){
+        print __CLASS__." Аутентификация запроса <br>";
+        $this->processRequest->process($req);
+    }
+}
+
+class StructureRequest extends DecorateProcess {
+    function process(RequestHelper $req){
+        print __CLASS__." Упорядочение данных запроса <br>";
+        $this->processRequest->process($req);
+    }
+}
+
+$req = new RequestHelper();
+
+$process = new StructureRequest(new AuthenticateProcess(new LogRequest(new MainProcess())));
+$process->process($req);
+
+// Декоратор на примепе фильтров к запросу, мы можем комбинировать очерёдность декораторов.
